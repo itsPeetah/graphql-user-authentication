71fb3a6496086d785494ae36e1763b3ceebc56a4
itsPeetah <pietro.g.moroni@gmail.com>
2022-01-16 21:46:31 +0100

    Made graphql context type session cookie compatible

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,7 @@
 import {Request, Response} from "express"
+import { SessionData } from "express-session"
 
 export type MyGraphQLContext = {
-    req : Request
+    req : Request & {session : SessionData & {userId?: number} } // session might be undefined on req, userId might be undefined on session (set with cookie)
     res : Response
 }
