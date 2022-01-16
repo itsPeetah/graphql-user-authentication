e7edac9caf6f1eff4bf1afc2eec59cf100df66db
itsPeetah <pietro.g.moroni@gmail.com>
2022-01-16 19:52:08 +0100

    Added session stuff to index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,9 @@ import {graphqlHTTP} from "express-graphql"
 import {buildSchema} from "type-graphql"
 import User from "./schema/entities/User"
 import UserResolver from "./schema/resolvers/user"
-
-console.log("Hello world!")
-
+import redis from "redis"
+import session from "express-session"
+import connectRedis from "connect-redis"
 
 const main =async () => {
 
@@ -21,9 +21,31 @@ const main =async () => {
     })
     console.log("DB Connection:", orm.name)
 
-
     const app = express()
     
+    const RedisStore = connectRedis(session)
+    const redisClient = redis.createClient()
+
+    // Applying express middleware in a specific order.
+    // Session will run before everything else.
+    app.use(
+        session({
+            name:"qid",
+            store: new RedisStore({
+                client:redisClient,
+                disableTouch:true,
+            }),
+            cookie:{
+                maxAge : 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
+                httpOnly: true,
+                sameSite: "lax", // csrf
+                secure: false, // cookie only works in https -> set true in production
+            },
+            secret:"kadfgkasfkasdofkapkdpofkkadfgkasfkasdofkapkdpofk",
+            resave:false,
+        })
+    )
+
     app.get("/", (_, res) => {
         res.send("I'm alive!")
     })
