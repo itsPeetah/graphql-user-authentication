d4c8dcb709a93719c11be11d97e8f541326b0cb2
itsPeetah <pietro.g.moroni@gmail.com>
2022-01-17 16:12:54 +0100

    Update _app.tsx
    fixed to match strict ts

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -10,7 +10,7 @@ const client = createClient({
   }
 })
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps } : any) {
   return (
     <URLQProvider value={client}>
       <ChakraProvider resetCSS theme={theme}>
