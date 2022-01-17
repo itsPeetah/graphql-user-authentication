beeb9685204baca0c7fe1a266ea5101859c13887
itsPeetah <pietro.g.moroni@gmail.com>
2022-01-17 16:12:26 +0100

    Generate gql code for register mutation

---
+++ web/src/generated/graphql.tsx
@@ -0,0 +1,96 @@
+import gql from 'graphql-tag';
+import * as Urql from 'urql';
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+};
+
+export type FieldError = {
+  __typename?: 'FieldError';
+  field: Scalars['String'];
+  message: Scalars['String'];
+};
+
+export type Mutation = {
+  __typename?: 'Mutation';
+  login: UserResponse;
+  register: UserResponse;
+};
+
+
+export type MutationLoginArgs = {
+  args: UsernamePasswordInput;
+};
+
+
+export type MutationRegisterArgs = {
+  args: UsernamePasswordInput;
+};
+
+export type Query = {
+  __typename?: 'Query';
+  allUsers: Array<User>;
+  getUser?: Maybe<UserResponse>;
+  me?: Maybe<User>;
+};
+
+
+export type QueryGetUserArgs = {
+  username: Scalars['String'];
+};
+
+export type User = {
+  __typename?: 'User';
+  _id: Scalars['Int'];
+  password: Scalars['String'];
+  username: Scalars['String'];
+};
+
+export type UserResponse = {
+  __typename?: 'UserResponse';
+  errors?: Maybe<Array<FieldError>>;
+  user?: Maybe<User>;
+};
+
+export type UsernamePasswordInput = {
+  password: Scalars['String'];
+  username: Scalars['String'];
+};
+
+export type RegisterMutationVariables = Exact<{
+  username: Scalars['String'];
+  password: Scalars['String'];
+}>;
+
+
+export type RegisterMutation = { __typename?: 'Mutation', register: { __typename?: 'UserResponse', user?: { __typename?: 'User', _id: number, username: string } | null | undefined, errors?: Array<{ __typename?: 'FieldError', field: string, message: string }> | null | undefined } };
+
+
+export const RegisterDocument = gql`
+    mutation Register($username: String!, $password: String!) {
+  register(args: {username: $username, password: $password}) {
+    user {
+      _id
+      username
+    }
+    errors {
+      field
+      message
+    }
+  }
+}
+    `;
+
+export function useRegisterMutation() {
+  return Urql.useMutation<RegisterMutation, RegisterMutationVariables>(RegisterDocument);
+};
