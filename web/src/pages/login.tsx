16d46316a8e78f975209eac013dae9bc7460222d
itsPeetah <pietro.g.moroni@gmail.com>
2022-01-17 17:01:38 +0100

    Removed unused import

--- web/src/pages/login.tsx
+++ web/src/pages/login.tsx
@@ -4,7 +4,7 @@ import { useRouter } from "next/router";
 import React from "react";
 import InputField from "../components/InputField";
 import { Wrapper } from "../components/Wrapper";
-import { useLoginMutation, useRegisterMutation } from "../generated/graphql";
+import { useLoginMutation } from "../generated/graphql";
 import { toErrorMap } from "../utils/toErrorMap";
 
 const Login : React.FC<{}> = ({}) => {
