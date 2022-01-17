61743e55eebce808fbc146baa973349e90e50388
itsPeetah <pietro.g.moroni@gmail.com>
2022-01-17 17:17:16 +0100

    Added "me" query

--- web/src/generated/graphql.tsx
+++ web/src/generated/graphql.tsx
@@ -81,6 +81,11 @@ export type RegisterMutationVariables = Exact<{
 
 export type RegisterMutation = { __typename?: 'Mutation', register: { __typename?: 'UserResponse', user?: { __typename?: 'User', _id: number, username: string } | null | undefined, errors?: Array<{ __typename?: 'FieldError', field: string, message: string }> | null | undefined } };
 
+export type MeQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type MeQuery = { __typename?: 'Query', me?: { __typename?: 'User', username: string, _id: number } | null | undefined };
+
 
 export const LoginDocument = gql`
     mutation Login($args: UsernamePasswordInput!) {
@@ -117,4 +122,16 @@ export const RegisterDocument = gql`
 
 export function useRegisterMutation() {
   return Urql.useMutation<RegisterMutation, RegisterMutationVariables>(RegisterDocument);
+};
+export const MeDocument = gql`
+    query Me {
+  me {
+    username
+    _id
+  }
+}
+    `;
+
+export function useMeQuery(options: Omit<Urql.UseQueryArgs<MeQueryVariables>, 'query'> = {}) {
+  return Urql.useQuery<MeQuery>({ query: MeDocument, ...options });
 };
