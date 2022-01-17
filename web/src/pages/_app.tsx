430857d319adf9ac4617b812ec3ac22f7104fb06
itsPeetah <pietro.g.moroni@gmail.com>
2022-01-17 17:44:13 +0100

    Added exchange-graphcache updates for me query

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,13 +1,49 @@
 import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react'
 import theme from '../theme'
+import {createClient, dedupExchange, fetchExchange, Provider as URLQProvider} from "urql"
+import { cacheExchange, Cache, QueryInput } from '@urql/exchange-graphcache'
+import { LoginMutation, MeDocument, MeQuery, RegisterMutation } from '../generated/graphql'
 
-import {createClient, Provider as URLQProvider} from "urql"
+function betterUpdateQuery<Result, Query>(cache: Cache, qi: QueryInput, result:any, fn: (r:Result, q:Query) => Query){
+  return cache.updateQuery(qi, data => fn(result, data as  any) as any)
+}
 
 const client = createClient({
   url:"http://localhost:5000/graphql",
   fetchOptions:{
     credentials:"include",
-  }
+  },
+  exchanges: [
+    dedupExchange,
+    cacheExchange({
+      // Custom data updates
+      updates: {
+        Mutation:{
+          login: (_result, args, cache, info ) => {
+            betterUpdateQuery<LoginMutation, MeQuery>(
+              cache,
+              {query:MeDocument},
+              _result,
+              (result, query) => {
+                if(result.login.errors) return query;
+                else return {me:result.login.user}
+              });
+          },
+          register: (_result, args, cache, info ) => {
+            betterUpdateQuery<RegisterMutation, MeQuery>(
+              cache,
+              {query:MeDocument},
+              _result,
+              (result, query) => {
+                if(result.register.errors) return query;
+                else return {me:result.register.user}
+              });
+          }
+        }
+      }
+    }),
+    fetchExchange
+  ]
 })
 
 function MyApp({ Component, pageProps } : any) {
